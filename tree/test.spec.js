c85c48781ca88032122954fffcc5f8a939e50386
yenobi <koshman.animys@gmail.com>
2018-04-02 23:31:10 +0300

    add test case for simple tree with 1 root Node and 3 one line child Nodes

--- tree/test.spec.js
+++ tree/test.spec.js
@@ -34,5 +34,21 @@ describe('basic parts of lab - tree and node class', () => {
         expect(tree.root).toBeTruthy();
         expect(tree.root.tree).toBe(tree);
         expect(tree.root.data).toBe('data');
+    });    
+});
+
+describe('simple tree with one root and three on line nodes', () => {
+    it('should create tree with root and 3 siblings', () => {
+        const tree = new Tree({name: 'root'});
+        const n1 = tree.add({name: 'n1'});
+        const n2 = tree.add({name: 'n2'});
+        const n3 = tree.add({name: 'n3'});
+        
+        expect(tree.count).toBe(4);
+        const root = tree.root;
+        expect(root.first).toBe(n1);
+        expect(root.last).toBe(n3);
+        expect(n1.next).toBe(n2);
+        expect(n3.prev).toBe(n2);
     });
 });
